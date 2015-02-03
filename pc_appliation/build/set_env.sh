d5d4f1428e06b1e850e851dc8ef409bcec445699
AVoloshyn <Voloshin.Anton@gmail.com>
2015-02-03 07:21:15 -0800

    Added: set enviroment script

---
+++ pc_appliation/build/set_env.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+#  Script %name:	set_build_env.sh %
+#  Instance:		ADI_WZ_1
+#  %version:		8 %
+#  Description:		
+#  %created_by:		uid65421 %
+#  %date_created:	Fri Nov 21 16:51:28 2014 %
+#
+#
+#  Description:     Script for settingup the build environment of the present project.
+#                   To start it, open a Linux shell, go to the present file path and
+#                   start it with
+#
+#                   source set_build_env.sh
+#
+#                   The script sets non-permanent build environment variables.
+#                   At the end, it prints what the user should do next.
+#
+echo "set the build environment"
